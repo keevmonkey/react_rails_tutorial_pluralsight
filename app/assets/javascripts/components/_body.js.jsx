6118cb7eeca794afbf383598620abd409ba84116
Kevin Hui <kevinhuitaylors@gmail.com>
2018-12-20 16:04:29 +0800

    created function to add new fruits

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -6,6 +6,28 @@ constructor(props){
     fruits: [],
     items: []
   };
+
+  this.handleFormSubmit = this.handleFormSubmit.bind(this)
+}
+
+handleFormSubmit(name, description){
+  let body = JSON.stringify({fruit: {name: name, description: description} })
+
+  fetch('/api/v1/fruits', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: body,
+  }).then((response) => {return response.json()
+  }).then((fruit) => {this.addNewFruit(fruit)
+  })
+}
+
+addNewFruit(fruit){
+  this.setState({
+    fruits: this.state.fruits.concat(fruit)
+  })
 }
 
 componentDidMount(){
@@ -18,10 +40,10 @@ componentDidMount(){
     .then((data) => {this.setState({ items: data }) });
 }
 
-
 render(){
   return(
     <div>
+      <NewFruit handleFormSubmit={this.handleFormSubmit}/>
       <AllFruits fruits={this.state.fruits} />
       <hr/>
       <AllItems items={this.state.items} />
