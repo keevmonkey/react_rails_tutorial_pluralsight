90b532d1850a4cbb9a675a2d9fb277d085c33f51
Kevin Hui <kevinhuitaylors@gmail.com>
2018-12-20 17:11:13 +0800

    built out button to edit individual fruit

--- app/assets/javascripts/components/_fruit.js.jsx
+++ app/assets/javascripts/components/_fruit.js.jsx
@@ -1,9 +1,26 @@
 class Fruit extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      editable: false
+    }
+    this.handleEdit = this.handleEdit.bind(this)
+  }
+
+  handleEdit(){
+    this.setState({
+      editable: !this.state.editable
+    })
+  }
+
   render(){
+    let name = this.state.editable ? <input type='text' ref={input => this.name = input} defaultValue={this.props.fruit.name}/> : <h3>{this.props.fruit.name}</h3>
+    let description = this.state.editable ? <input type='text' ref={input => this.description = input} defaultValue={this.props.fruit.description}/> : <p>{this.props.fruit.description}</p>
     return(
       <div>
-        <h1>{this.props.fruit.name}</h1>
-        <p>{this.props.fruit.description}</p>
+        {name}
+        {description}
+        <button onClick={() => this.handleEdit()}>{this.state.editable? 'submit' : 'Edit'}</button>
         <button onClick={() => this.props.handleDelete(this.props.fruit.id)}>Delete</button>
       </div>
     )
